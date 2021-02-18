a266c32021a30cdb8830ed72d6c536cf2bee5965
Eddie Phimansone <ephimansone@gmail.com>
2021-02-18 14:09:12 -0500

    initiall server setup | plots GET and POST work

---
+++ models/plot.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const plotSchema = new Schema(
+  {
+    plot_name: {
+      type: String,
+      trim: true,
+      required: "Enter a plot name"
+    },
+    plot_rows: {
+      type: Number,
+      required: "Enter the amount of rows"
+    },
+    plot_columns: {
+      type: Number,
+      required: "Enter the amount of columns"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
+
+const Plot = mongoose.model("Plot", plotSchema);
+
+module.exports = Plot;
