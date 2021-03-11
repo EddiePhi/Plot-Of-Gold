841024fadd3cd9c91085874e25543fe547dbccc7
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-03-11 07:41:38 -0500

    plant modal work commit before main pull

--- client/src/components/Modals/BaseModal.js
+++ client/src/components/Modals/BaseModal.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./index.css";
-import { Modal } from "react-bootstrap";
+import { Modal, Col, Row, Container } from "react-bootstrap";
 import OurButton from "../OurButton";
+import PlantModalItem from "./PlantModalItem";
 
 function BaseModal() {
   const [show, setShow] = useState(true);
@@ -10,7 +11,7 @@ function BaseModal() {
   const handleShow = () => setShow(true);
   return (
     <>
-      <Modal show={show} onHide={() => setShow(false)} ClassName="modal-90w">
+      <Modal show={show} onHide={() => setShow(false)} ClassName="custModal">
         <Modal.Header>
           <Modal.Title id="example-custom-modal-styling-title">
             Plant-i-Dex
@@ -18,15 +19,13 @@ function BaseModal() {
           <OurButton value="Close" onClick={handleClose} />
         </Modal.Header>
         <Modal.Body>
-          <p>
-            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-            ipsam atque a dolores quisquam quisquam adipisci possimus
-            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-            deleniti rem!
-          </p>
+          <PlantModalItem />
+          <PlantModalItem />
+          <PlantModalItem />
+          <PlantModalItem />
+          <PlantModalItem />
+          <PlantModalItem />
+          <PlantModalItem />
         </Modal.Body>
       </Modal>
     </>
