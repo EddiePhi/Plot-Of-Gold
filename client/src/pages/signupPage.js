7daa887b9a45012772d12b3e2d5a0556002b27c9
Dlabins <DLABINS1895@GMAIL.COM>
2021-03-11 21:11:46 -0500

    sign up and login page barebones minus functionality and styling

--- client/src/pages/signupPage.js
+++ client/src/pages/signupPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import OurForm from "../components/OurForm";
+import OurButtonProps from "../components/OurButtonProps";
 
 function SignupPage() {
   return (
     <>
-      <h1>SignupPage</h1>
+     <div className="form-control text-center">
+      <h1 className="form-control text-center">Signup Page</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <OurForm></OurForm>
+      <OurButtonProps></OurButtonProps>
+      <br></br>
+      <br></br>
+      <div className="level has-text-centered">
+        <a className="level-item has-text-white has-text-centered" href="/login" style={{margin: '0px'}}>Already registered? Click here to login.</a>
+      </div>
+    </div>
     </>
   );
 }
